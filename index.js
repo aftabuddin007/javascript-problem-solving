a7a716a7495b19273fe1f5004e5a54090d12c2c2
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-07 23:59:48 +0600

    solve problem 2

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ function reverseString(str){
     return str.split("").reverse().join("")
 }
 console.log(reverseString('hello'))
+// problem 2
+function getCount(str){
+    var vowelsCount = 0;
+    const vowels = ['a','e','i','o','u']
+    for(let char of str.toLowerCase()){
+        if(vowels.includes(char)){
+            vowelsCount++
+        }
+    }
+    return vowelsCount;
+
+}
+console.log(getCount('hELLo WorlD'))
