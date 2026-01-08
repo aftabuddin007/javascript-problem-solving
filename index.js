ef49b6f91afebe77b89ea9d09e9a8bc08669fea3
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-08 12:50:06 +0600

    fix all issues

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // problem 1
-// function reverseString(str){
-//     return str.split("").reverse().join("")
-// }
-// console.log(reverseString('hello'))
+function reverseString(str){
+    return str.split("").reverse().join("")
+}
+console.log(reverseString('hello'))
 
 
 
@@ -10,18 +10,18 @@
 
 
 // problem 2
-// function getCount(str){
-//     var vowelsCount = 0;
-//     const vowels = ['a','e','i','o','u']
-//     for(let char of str.toLowerCase()){
-//         if(vowels.includes(char)){
-//             vowelsCount++
-//         }
-//     }
-//     return vowelsCount;
+function getCount(str){
+    var vowelsCount = 0;
+    const vowels = ['a','e','i','o','u']
+    for(let char of str.toLowerCase()){
+        if(vowels.includes(char)){
+            vowelsCount++
+        }
+    }
+    return vowelsCount;
 
-// }
-// console.log(getCount('programming'))
+}
+console.log(getCount('programming'))
 
 
 
@@ -29,28 +29,28 @@
 
 
 //problem 3
-// function isPalindrome(str){
-//     const lowerString = str.toLowerCase();
-//     const reversedStr = lowerString.split("").reverse().join('')
-//     return lowerString === reversedStr
-// }
-// console.log(isPalindrome("madam")); 
-// console.log(isPalindrome("hello")); 
+function isPalindrome(str){
+    const lowerString = str.toLowerCase();
+    const reversedStr = lowerString.split("").reverse().join('')
+    return lowerString === reversedStr
+}
+console.log(isPalindrome("madam")); 
+console.log(isPalindrome("hello")); 
 
 
 
 
 // problem 4
-// function getMaxNumber(arr){
-//     let max = arr[0]
-//     for(let i =1;i<arr.length;i++){
-//         if(arr[i] > max){
-//             max = arr[i];
-//         }
-//     }
-//     return max;
-// }
-// console.log(getMaxNumber([3,9,1,4,0]))
+function getMaxNumber(arr){
+    let max = arr[0]
+    for(let i =1;i<arr.length;i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(getMaxNumber([3,9,1,4,0]))
 
 
 
@@ -58,31 +58,31 @@
 
 
 // problem 5
-// function duplicateArray(arr){
-//     let result = [];
-//     for(let num of arr){
-//         if(!result.includes(num)){
-//             result.push(num)
-//         }
+function duplicateArray(arr){
+    let result = [];
+    for(let num of arr){
+        if(!result.includes(num)){
+            result.push(num)
+        }
 
-//     }
-//     return result
-// }
-// console.log(duplicateArray([1,2,2,34,4,5]))
+    }
+    return result
+}
+console.log(duplicateArray([1,2,2,34,4,5]))
 
 
 
 
 
 // problem 6
-// function sumArray(arr){
-//     let sum = 0;
-//     for(let num of arr){
-//         sum += num
-//     }
-//     return sum
-// }
-// console.log(sumArray([1,2,3,4]))
+function sumArray(arr){
+    let sum = 0;
+    for(let num of arr){
+        sum += num
+    }
+    return sum
+}
+console.log(sumArray([1,2,3,4]))
 
 
 
@@ -90,16 +90,16 @@
 
 
 // problem 7
-// function findEvenNum(arr){
-//     let number = [];
-//     for(let n of arr){
-//         if(n%2 ===0){
-//             number.push(n)
-//         }
-//     }
-//     return number;
-// }
-// console.log(findEvenNum([1,2,3,4,5,6]))
+function findEvenNum(arr){
+    let number = [];
+    for(let n of arr){
+        if(n%2 ===0){
+            number.push(n)
+        }
+    }
+    return number;
+}
+console.log(findEvenNum([1,2,3,4,5,6]))
 
 
 
@@ -107,17 +107,17 @@
 
 
 // problem 8
-// function capitalizeWord(str){
-//     const words = str.split(' ')
-//     let result = []
-//     for (let w of words){
-//         const capitalWord = w[0].toUpperCase()+w.slice(1)
-//         result.push(capitalWord)
-//     }
-//     return result.join(' ')
+function capitalizeWord(str){
+    const words = str.split(' ')
+    let result = []
+    for (let w of words){
+        const capitalWord = w[0].toUpperCase()+w.slice(1)
+        result.push(capitalWord)
+    }
+    return result.join(' ')
 
-// }
-// console.log(capitalizeWord('hello world'))
+}
+console.log(capitalizeWord('hello world'))
 
 
 
@@ -125,15 +125,15 @@
 
 
 // problem 9
-// function factorial(num){
-//     let result = 1
-//     for(let i=1;i<=num;i++){
-//         result *= i;
+function factorial(num){
+    let result = 1
+    for(let i=1;i<=num;i++){
+        result *= i;
 
-//     }
-//     return result;
-// }
-// console.log(factorial(5))
+    }
+    return result;
+}
+console.log(factorial(5))
 
 
 
